28bb22e6708094edd9b1bab846c923e16d4d57dd
shivanshupathak001 <shivanshupathak1129@gmail.com>
2026-06-07 11:59:57 +0530

    Added (The program doing some arithmetic operations between 2 numbers taken from the user)

---
+++ Pr2.c
@@ -0,0 +1,15 @@
+//WAP to do arithmetic operations in two numbers 
+#include<stdio.h>
+int main() {
+    int a , b ;
+    printf("Enter First Number \n");
+    scanf("%d" , &a);
+    printf("Enter Second Number \n");
+    scanf("%d" , &b);
+    printf("The sum of your given numbers is %d \n", a+b);
+    printf("The product of your given numbers is %d \n", a*b);
+    printf("The difference of your given numbers is %d \n", a-b);
+    printf("The qoutient when b is divided by a is %d \n", a/b);
+    printf("The remainder when b is divided by a is %d \n", a%b);
+    return 0;
+}

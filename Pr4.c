f49366197759372d7317e219078ab40541f11d3d
shivanshupathak001 <shivanshupathak1129@gmail.com>
2026-06-08 20:52:26 +0530

    Added (The program performing various arithmetic operations on random numbers)

---
+++ Pr4.c
@@ -0,0 +1,12 @@
+//WAP to understand the arithmetic instructions , relational operator and logical operator 
+#include<stdio.h>
+int main(){
+    printf("%d\n",7%4);
+    printf("%d\n",-7%4);
+    printf("%d\n",5==5);
+    printf("%d\n",7>8);
+    printf("%d\n",2!=2);
+    printf("%d\n",4>3 && 4>5);
+    printf("%d\n",4>3 || 4>5);
+    return 0;
+}

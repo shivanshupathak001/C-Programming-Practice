9fef078697f50abc7dc521692461b5ee33f7827e
shivanshupathak001 <shivanshupathak1129@gmail.com>
2026-06-10 21:35:19 +0530

    Added (The program finding the average of 3 numbers given by the user)

---
+++ Pr7.c
@@ -0,0 +1,9 @@
+// WAP to find the average of 3 numbers 
+#include<stdio.h>
+int main() {
+    int a , b , c ;
+    printf("Enter Three Numbers \n");
+    scanf("%d %d %d", &a , &b , &c);
+    printf("The average of these three numbers is %d" , (a+b+c)/3);
+    return 0;
+}

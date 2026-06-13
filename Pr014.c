e0b3fd7d8b6ee08a51925e2d95792422b39db082
shivanshupathak001 <shivanshupathak1129@gmail.com>
2026-06-13 21:35:43 +0530

    Added the program checking if a number entered by the user is natural or not

---
+++ Pr014.c
@@ -0,0 +1,15 @@
+//Program 14 (13th June 2026)
+// WAP to check if a given number is natural or not
+#include<stdio.h>
+int main() {
+    int num;
+    printf("Enter a Number \n");
+    scanf("%d",&num);
+    if(num>=1) {
+        printf("Natural Number \n");
+    }
+    else {
+        printf("Not a Natural Number \n");
+    }
+    return 0;
+} 

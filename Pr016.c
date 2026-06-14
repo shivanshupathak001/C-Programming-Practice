3e597ce415038f171554d250285d0cc7a42da8fa
shivanshupathak001 <shivanshupathak1129@gmail.com>
2026-06-14 20:41:14 +0530

    Added the program printing hello world 10 times using while-loop

---
+++ Pr016.c
@@ -0,0 +1,11 @@
+//Program 16 (14th June 2026)
+// WAP to Print Hello World 10 times using While loop
+#include<stdio.h>
+int main() {
+    int i=1;
+    while(i<=10){
+        printf("Hello World \n");
+        i++;
+    }
+    return 0;
+}

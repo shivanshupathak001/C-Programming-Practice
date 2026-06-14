19e536b5968386b38743b99302ed8f0490664feb
shivanshupathak001 <shivanshupathak1129@gmail.com>
2026-06-14 20:38:02 +0530

    Added the program printing the numbers between 1 to 10 using for-loop

---
+++ Pr015.c
@@ -0,0 +1,9 @@
+//Program 15 (14th June 2026)
+// WAP to Print Numbers between 1 to 10 using For loop
+#include<stdio.h>
+int main() {
+    for(int i=1;i<=10;i++){
+        printf("%d \n",i);
+    }
+    return 0;
+}

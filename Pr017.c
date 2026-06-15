13b4503244c41309941eb60ee1de7741311ce6c9
shivanshupathak001 <shivanshupathak1129@gmail.com>
2026-06-15 20:01:32 +0530

    Added the program printing hello world 5 times using do-while loop

---
+++ Pr017.c
@@ -0,0 +1,12 @@
+//Program 17 (15th June 2026)
+// WAP to print Hello World 5 times using do-while loop
+#include<stdio.h>
+int main() {
+    int i=1;
+    do {
+        printf("Hello World \n");
+        i++;
+    }
+    while(i<=5);
+    return 0;
+}

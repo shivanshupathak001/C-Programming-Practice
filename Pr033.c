ce8b127ae10e86397a432a291f1999dbdfd9c5df
shivanshupathak001 <shivanshupathak1129@gmail.com>
2026-06-23 21:04:42 +0530

    Added the program printing hello world 3 times by calling the function

---
+++ Pr033.c
@@ -0,0 +1,13 @@
+//Program 33 (23rd June 2026)
+// WAP to print "Hello" 3 times from function calling
+#include<stdio.h>
+void printhello();
+int main() {
+    printhello();
+    printhello();
+    printhello();
+    return 0;
+}
+void printhello() {
+    printf("Hello \n");
+}

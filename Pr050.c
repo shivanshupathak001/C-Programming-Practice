73933c1cbc7866a920d7a5673fdad6dc1cfa139c
shivanshupathak001 <shivanshupathak1129@gmail.com>
2026-07-01 21:45:25 +0530

    Added the program understanding the basic concept of pointers

---
+++ Pr050.c
@@ -0,0 +1,22 @@
+//Program 50 (1st July 2026)
+//WAP to understand the basic concept of pointers
+#include<stdio.h>
+int main() {
+    int *ptr;
+    int x;
+    ptr=&x;
+    *ptr=0;
+
+    printf("x= %d\n",x);
+    printf("*ptr= %d\n",*ptr);
+
+    *ptr+=5;
+    printf("x= %d\n",x);
+    printf("*ptr= %d\n",*ptr);
+
+    (*ptr)++;
+    printf("x= %d\n",x);
+    printf("*ptr= %d\n",*ptr);
+
+    return 0;
+}

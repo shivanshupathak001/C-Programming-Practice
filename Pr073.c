ebc868740ab73302a8771b23e428fc9c1c52f091
shivanshupathak001 <shivanshupathak1129@gmail.com>
2026-07-13 23:24:41 +0530

    Added the program asking the user to enter their first name and printing it back to them

---
+++ Pr073.c
@@ -0,0 +1,10 @@
+//Program 73 (13th July 2026)
+//WAP to ask the user to enter their first name and print it back to them
+#include<stdio.h>
+int main() {
+    char firstName[50];
+    printf("Enter Your First Name :-\n");
+    scanf("%s",firstName);
+    printf("Your Name is %s\n",firstName);
+    return 0;
+}
